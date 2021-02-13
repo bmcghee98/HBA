ccff8e0c65c7bcf7c07461fbb5d4537af8ac1f53
coinnpurse <47498733+coinnpurse@users.noreply.github.com>
2021-02-12 20:42:51 -0800

    Fixed Birthday class and mem leaks

--- app/src/main/java/com/coinnpursecoding/hba/Birthday.java
+++ app/src/main/java/com/coinnpursecoding/hba/Birthday.java
@@ -3,10 +3,12 @@ package com.coinnpursecoding.hba;
 public class Birthday {
     private String personName;
     private long longDate;
+    private String dbDate;
 
-    Birthday(String name, long date){
+    Birthday(String name, long date, String formDate){
         personName = name;
         longDate = date;
+        dbDate = formDate;
     }
 
     Birthday(long date){
@@ -14,13 +16,11 @@ public class Birthday {
         personName = "";
     }
 
-    public String getName(){
-        return personName;
-    }
+    public String getName(){return personName; }
 
-    public long getLongDate(){
-        return longDate;
-    }
+    public long getLongDate(){ return longDate; }
+
+    public String getDbDate(){ return dbDate; }
 
 
 }
